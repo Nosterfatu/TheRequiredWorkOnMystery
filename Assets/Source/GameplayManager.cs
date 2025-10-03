0836bc1111fadbfb5ae7193d8ee9cba47f14fc2c
M.Markov <nosterfatu@gmail.com>
2025-10-03 19:19:04 +0200

    Added autosave progression

--- Assets/Source/GameplayManager.cs
+++ Assets/Source/GameplayManager.cs
@@ -3,16 +3,19 @@
     public class GameplayManager
     {
         private readonly SidePanelController _sidePanelController;
+        private readonly IProgressionManager _progressionManager;
 
-        public GameplayManager(FieldController fieldController, SidePanelController sidePanelController)
+        public GameplayManager(FieldController fieldController, SidePanelController sidePanelController, IProgressionManager progressionManager)
         {
             _sidePanelController = sidePanelController;
+            _progressionManager = progressionManager;
             fieldController.FieldChanged+= OnFieldChanged;
         }
 
         private void OnFieldChanged(GameSave save)
         {
             _sidePanelController.Update(save);
+            _progressionManager.SetLevel(save);
         }
     }
 }
